6e5e8c95a0d4a33f1ab21f459f7a58483d27be05
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-01-06 09:56:57 -0800

    Test case one

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -3,6 +3,8 @@
 
 import unittest
 import pycodestyle
+import MYSQLdb
+import os
 from models.engine.db_storage import DBStorage
 
 
@@ -25,6 +27,36 @@ class TestDBStorage(unittest.TestCase):
         self.assertIsNotNone(DBStorage.delete.__doc__)
         self.assertIsNotNone(DBStorage.reload.__doc__)
 
+    def test_new_and_save(self):
+        """ This is a methid that test the new and save method """
+        db = MYSQLdb.connect(
+                user=os.getenv('HBNB_MYSQL_USER'),
+                host=os.getenv('HBNB_MYSQL_HOST'),
+                password=os.getenv('HBNB_MYSQL_PWD'),
+                dbname=os.getenv('HBNB_MYSQL_DB'),
+                port=3306)
+        new_user = User(**{
+            "first_name": "Victor",
+            "last_name": "Chukwuwmka",
+            "email": "onwuegbuchulemvic02@gmail.com",
+            "password": "12345"})
+        cur = db.cursor()
+        cur.execute('SELECT COUNT(*) FROM users')
+        counter = cur.fetchall()
+        cur.close()
+        db.close()
+        new_user.save()
+        db = MySQLdb.connect(user=os.getenv('HBNB_MYSQL_USER'),
+                             host=os.getenv('HBNB_MYSQL_HOST'),
+                             passwd=os.getenv('HBNB_MYSQL_PWD'),
+                             port=3306,
+                             db=os.getenv('HBNB_MYSQL_DB'))
+        cur = db.cursor()
+        cur.execute('SELECT COUNT(*) FROM users')
+        new_count = cur.fetchall()
+        self.assertEqual(new_count[0][0], old_count[0][0] + 1)
+        cur.close()
+        db.close()
 
 if __name__ == "__main__":
     unittest.main()
