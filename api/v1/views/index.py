59d133fcf0d278bdf35965009a2527d647b06e37
Johnson adeola <johnsamxy@gmail.com>
2024-01-08 09:48:08 +0100

    new task

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,24 @@
 """ returns json status for app_views routes of the Flask app """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', return_slashes=False)
 def status():
     """ return json status: OK """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat_count():
+    """ endpoint that retrieves the # of each objects by type """
+    count_stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(count_stats)
