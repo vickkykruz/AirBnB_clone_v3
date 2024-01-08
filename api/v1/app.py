b2aba451cdacfd508a3730f125467f66a4aa69fd
Johnson adeola <johnsamxy@gmail.com>
2024-01-08 09:44:03 +0100

    quick fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,7 @@ from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = int(os.getenv('HBNB_API_PORT', '5000'))
 
